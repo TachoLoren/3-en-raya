2500050ce5d804c963bfc083bd1ea15257838311
TachoLoren <skaterloren@hotmail.com>
2020-05-25 06:32:36 +0200

    first commit

---
+++ src/tateti/Ficha.java
@@ -0,0 +1,21 @@
+package tateti;
+// Beans
+public class Ficha {
+    private String ficha;
+
+    public Ficha() {
+    }
+
+    public Ficha(String ficha) {
+        this.ficha = ficha;
+    }
+
+    public String getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(String ficha) {
+        this.ficha = ficha;
+    }
+   
+}
