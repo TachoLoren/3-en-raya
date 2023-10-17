aedafc0af60609a199a93722a4937a7c2acd9e72
TachoLoren <skaterloren@hotmail.com>
2023-10-17 10:06:05 +0200

    seguimos testeando

--- src/tateti/test.java
+++ src/tateti/test.java
@@ -2,7 +2,7 @@ public class Test {
 
    public static void main(String[] args) {
 
-      System.out.println("Hola Mundo testeando git");
+      System.out.println("Hola Mundo testeando git en vscode");
 
    }
 
